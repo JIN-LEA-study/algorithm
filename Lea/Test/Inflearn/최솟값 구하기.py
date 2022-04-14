44aac7076ba08ba0c372e387e9d74ffbbb40b179
leaisrevolution <leaisrevolution@gmail.com>
2022-04-14 21:47:28 +0900

    알고리즘 최솟값 구하는 방법 (두가지 예시)

--- Lea/Test/Inflearn/최솟값 구하기.py
+++ Lea/Test/Inflearn/최솟값 구하기.py
@@ -8,3 +8,12 @@ for i in range(len(arr)): #arr의 길이
     if arr[i] < arrMin:
         arrMin=arr[i]
 print(arrMin)
+
+
+arr = [5, 3, 7, 2, 5, 2, 6]
+arrMin=arr[0] #무한대값
+
+for i in range(1, len(arr)): #arr의 길이
+    if arr[i] < arrMin:
+        arrMin=arr[i]
+print(arrMin)
