4e130b9a52bbd3dddff893db0b270cdd8cc32d1f
leaisrevolution <leaisrevolution@gmail.com>
2022-04-16 22:06:19 +0900

    알고리즘 공부 업데이트

--- Lea/Test/Baekjoon/팩토리얼.py
+++ Lea/Test/Baekjoon/팩토리얼.py
@@ -13,3 +13,25 @@ for i in range(1, n+1):
 
 print(sum)
 
+
+
+# 팩토리얼 재귀 함수식으로 풀기
+
+def factorial(n):
+    result = 1
+    if n > 0 : #0일 경우 그냥 result는 1
+        result = n * factorial(n-1)
+    return result
+
+n = int(input())
+print(factorial(n))
+
+
+def factorial2(n):
+    if n == 0:
+        return 1
+    return n * factorial2(n-1)
+
+n = int(input())
+print(factorial2(n))
+
--- Lea/Test/Inflearn/대표값.py
+++ Lea/Test/Inflearn/대표값.py
@@ -1,8 +1,17 @@
+
+
 '''
+
+1. 문제를 나눠쓰기
+2. 하루 3문제를 못풀면 잘못된 것 (두호가 내준 숙제 기준)
+
+
 대표값
 
-N명의 학생의 수학점수가 주어집니다. N명의 학생들의 평균(소수 첫째자리 반올림)을 구하고, N명의 학생 중 평균에 가장 가까운 학생은 몇 번째 학생인지 출력하는 프로그램을 작성하세 요.
-평균과 가장 가까운 점수가 여러 개일 경우 먼저 점수가 높은 학생의 번호를 답으로 하고, 높 은 점수를 가진 학생이 여러 명일 경우 그 중 학생번호가 빠른 학생의 번호를 답으로 합니다.
+N명의 학생의 수학점수가 주어집니다. N명의 학생들의 평균(소수 첫째자리 반올림)을 구하고, N명의 학생 중 평균에 가장 가까운 학생은 몇 번째 학생인지 출력하는 프로그램을 작성하세요.
+
+평균과 가장 가까운 점수가 여러 개일 경우 먼저 점수가 높은 학생의 번호를 답으로 하고,
+높은 점수를 가진 학생이 여러 명일 경우 그 중 학생번호가 빠른 학생의 번호를 답으로 합니다.
 
 ▣ 입력설명
 첫줄에 자연수 N(5<=N<=100)이 주어지고, 두 번째 줄에는 각 학생의 수학점수인 N개의 자연 수가 주어집니다. 학생의 번호는 앞에서부터 1로 시작해서 N까지이다.
@@ -23,20 +32,64 @@ N명의 학생의 수학점수가 주어집니다. N명의 학생들의 평균(�
 여기서 점수가 높은 75(7번), 75(9번)이 답이 될 수 있고, 75점이 두명이므로 학생번호가 빠른 7번이 답이 됩니다.'''
 
 
-n = int(input())
-all_score = list(map(int, input().split()))
-ave = round(sum(all_score)/n)
-min = 21470000000 #4바이트
 
-for idx, x in enumerate(all_score): #idx = 인덱스, x = list 값
+
+#인프런코드
+
+num = int(input())
+scores = list(map(int, input().split()))
+ave = round(sum(scores))
+
+for idx, x in enumerate(scores):
     tmp = abs(x-ave)
-    if tmp < min :
+    if tmp < min:
         min = tmp
         score = x
         res = idx + 1
     elif tmp == min :
         if x > score:
             score = x
-            res=idx+1
-print (ave, res)
+            res = idx + 1
+
+
+
+
+
+#1
+
+n = int(input())
+scores = list(map(int, input().split()))
+average = round(sum(scores)/n) #평균값 구하기
+min_value = 999999999
+min_index = -1
+
+
+#2 평균값과 점수 거리 구하기
+
+# x = 70
+# average = 60
+
+# if x > average:
+#     result = x-average
+# else:
+#     result = average-x
+
+for i in range(len(scores)):
+    if scores[i] > average:
+        diff = scores[i] - average
+    else:
+        diff = average-scores[i]
+    if min_value > diff:
+        min_value = diff
+        min_index = i #최소값일 때 i(인덱스)
+    elif min_value == diff and scores[min_index] < scores[i]:
+        min_index = i
+
+print(min_index+1)
+
+
+# print(min_index+1) #인덱스는 0부터 시작하니까, 번째의 값을 구할 경우 +1
+
+
+
 
