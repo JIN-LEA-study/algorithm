ddef494d92b9e0d0f6f456c2275cb7caf5a707d4
HongJoonHyung <sesa0802@naver.com>
2022-05-06 22:17:44 +0900

    5월 6일 문제풀이

--- Joon/JS/baekjoon/2447_별찍기-10.js
+++ Joon/JS/baekjoon/2447_별찍기-10.js
@@ -10,10 +10,10 @@ function printStar(i, j) {
   if (i % 3 === 1 && j % 3 === 1) {
     str += " ";
   } else {
-    if (Math.floor(i / 3) === 1 && Math.floor(j / 3) === 1) {
-      str += " ";
-    } else {
+    if (Math.floor(i / 3) === 0 && Math.floor(j / 3) === 0) {
       str += "*";
+    } else {
+      printStar(Math.floor(i / 3), Math.floor(j / 3));
     }
   }
 }
