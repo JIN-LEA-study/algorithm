e86aa107b8556561509c28e4f1080cf5a1e256d9
“lea” <leaisrevolution@gmail.com>
2022-05-22 22:32:21 +0900

    inflearn 최소힙

--- Lea/Test/Inflearn/최소힙.py
+++ Lea/Test/Inflearn/최소힙.py
@@ -8,7 +8,7 @@ import heapq as hq
 a = []
 while True:
   n = int(input())
-  if n == 1:
+  if n == -1:
     break
   if n == 0:
     if len(a) == 0:
@@ -17,3 +17,71 @@ while True:
       print(hq.heappop(a))
   else:
     hq.heappush(a,n)
+
+
+# anthoer code 내장함수 안쓰고 코드 구현
+
+class Heap: # min heap
+    def __init__(self):
+        self.values = [0]
+
+    def is_empty(self):
+        return len(self.values) == 1
+
+    def push(self, value):
+        index = len(self.values)
+        self.values.append(value)
+        while index > 1:
+            if self.values[index // 2] > self.values[index]:
+                self.swap(index, index // 2)
+            else:
+                break
+            index = index // 2
+
+    def swap(self, index1, index2):
+        self.values[index1], self.values[index2] = self.values[index2], self.values[index1]
+
+    def pop(self):
+        min_value = self.values[1]
+        self.values[1] = self.values.pop()
+        index = 1
+        n = len(self.values) - 1
+        while True:
+            if n < index * 2: # no child
+                break
+            elif n < index * 2 + 1: # only left child
+                if self.values[index] > self.values[index * 2]:
+                    self.swap(index, index * 2)
+                    index = index * 2
+                else:
+                    break
+            else: # have both children
+                if self.values[index] > self.values[index * 2] or self.values[index] > self.values[index * 2 + 1]:
+                    if self.values[index * 2] < self.values[index * 2 + 1]:
+                        self.swap(index, index * 2)
+                        index = index * 2
+                    else:
+                        self.swap(index, index * 2 + 1)
+                        index = index * 2 + 1
+                else:
+                    break
+
+        return min_value
+
+heap = Heap()
+
+while True:
+    n = int(input())
+    if n == -1:
+        break
+    elif n == 0:
+        if heap.is_empty():
+            print(-1)
+        else:
+            print(heap.pop())
+    else:
+        heap.push(n)
+
+
+
+
