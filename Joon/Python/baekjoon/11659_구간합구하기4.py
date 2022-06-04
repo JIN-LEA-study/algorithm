a1b46fc96461cba2908436f2b58a9f4952ce9ad4
HongJoonHyung <sesa0802@naver.com>
2022-06-05 00:38:38 +0900

    6월 4일 문제 풀이

--- Joon/Python/baekjoon/11659_구간합구하기4.py
+++ Joon/Python/baekjoon/11659_구간합구하기4.py
@@ -2,7 +2,13 @@ import sys
 input = sys.stdin.readline
 N, M = map(int,input().strip().split());
 arr = list(map(int,input().split()))
+prefix_sum = [0]
+
+temp = 0
+for x in arr:
+    temp += x
+    prefix_sum.append(temp)
 
 for _ in range(M):
     i, j = map(int,input().strip().split());
-    print(sum(arr[i-1:j]))
+    print(prefix_sum[j] - prefix_sum[i-1])
