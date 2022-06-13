c62e05359686bbc47d494444571481a967de280b
HongJoonHyung <sesa0802@naver.com>
2022-06-14 01:33:12 +0900

    6월 13일 문제 풀이

---
+++ Joon/Python/baekjoon/9461_파도반수열.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+dp = [0,1,1]
+for i in range(3,100):
+    dp.append(dp[i-3] + dp[i-2])
+
+T = int(input().strip())
+for _ in range(T):
+    x = int(input())
+    print(dp[x])
