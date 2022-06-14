48adb049eccb235c6a1bde31544958a30ee630f4
HongJoonHyung <sesa0802@naver.com>
2022-06-15 02:57:56 +0900

    6월 14일 문제 풀이

---
+++ Joon/Python/baekjoon/1992_쿼드트리.py
@@ -0,0 +1,31 @@
+from unittest import result
+
+
+n = int(input())
+tree = []
+
+for _ in range(n):
+    tree.append(list(map(int,input())))
+
+result = []
+
+def quad_tree(x,y,n):
+    global result  
+    color = tree[x][y]
+
+    for i in range(x,x+n):
+        for j in range(y,y+n):
+            if color != tree[i][j]:
+                result.append("(")
+                quad_tree(x,y,n//2)
+                quad_tree(x,y+n//2,n//2)
+                quad_tree(x+n//2,y,n//2)
+                quad_tree(x+n//2, y+n//2, n//2)
+                result.append(")")
+                return
+    result.append(color)
+
+
+
+quad_tree(0,0,n)
+print("".join(map(str,result)))
--- Joon/Python/baekjoon/9461_파도반수열.py
+++ Joon/Python/baekjoon/9461_파도반수열.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 
 dp = [0,1,1]
-for i in range(3,100):
+for i in range(3,101):
     dp.append(dp[i-3] + dp[i-2])
 
 T = int(input().strip())
